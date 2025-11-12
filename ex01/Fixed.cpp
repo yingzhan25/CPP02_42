0509646ff4fe3ecf8f61b7d88d34bfe17d31f8e8
yingzhan25 <3146589226@qq.com>
2025-11-12 12:31:17 +0100

    add ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yingzhan <yingzhan@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 17:54:47 by yingzhan          #+#    #+#             */
+/*   Updated: 2025/11/12 12:30:47 by yingzhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed() : _rawValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::Fixed(int const value) : _rawValue(value << _FracBits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const value) :_rawValue(roundf(value * 256))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->_rawValue = other._rawValue;
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits(void)const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_rawValue);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_rawValue = raw;
+}
+
+float	Fixed::toFloat(void)const
+{
+	return (roundf(_rawValue / 256));
+}
+
+int		Fixed::toInt(void)const
+{
+	return (_rawValue >> _FracBits);
+}
+
+std::ostream&	operator<<(std::ostream &os, const Fixed& fixed)
+{
+	os << fixed.toFloat();
+	return (os);
+}
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: yingzhan <yingzhan@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:13:25 by yingzhan          #+#    #+#             */
-/*   Updated: 2025/11/12 11:43:12 by yingzhan         ###   ########.fr       */
+/*   Updated: 2025/11/12 12:29:18 by yingzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,17 @@ class Fixed
 	public:
 		Fixed();
 		Fixed(const Fixed& other);
+		Fixed(int const value);
+		Fixed(float const value);
 		Fixed&	operator=(const Fixed& other);
 		~Fixed();
+
 		int		getRawBits(void)const;
 		void	setRawBits(int const raw);
+		float	toFloat(void)const;
+		int		toInt(void)const;
 };
+
+std::ostream&	operator<<(std::ostream &os, const Fixed& fixed);
 
 #endif
